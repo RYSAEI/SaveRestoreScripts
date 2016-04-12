fcc53cf86f5db8bfa686fac381b94bb202c04b2c
Bruno Cascio <brunocascio@gmail.com>
2016-04-12 19:14:09 -0300

    Refactoring. Soporte para pasar el directorio de configuracione

--- restore.sh
+++ restore.sh
@@ -1,29 +1,61 @@
 #!/bin/bash
-SOCKETS_DIR=$(ps aux | grep -oP "/tmp/pycore.[0-999999999999999].+?(?=/)" | head -n 1)
-CONFIGS_DIR='configs'
 
-rm start.sh
-find "$CONFIGS_DIR" -maxdepth 1 -type f | while read file; do
-	FILENAME=$(basename "$file")
-	NODE=${FILENAME%.*}
-	# Tenemos que decidir cuándo es un router y cuándo no (en los .run los routers empiezan con "!"):
-	first_line=$(head -1 $file)
-	if [ "$first_line" == "!" ]
-	then
-		ARGUMENT="/usr/sbin/vcmd -c $SOCKETS_DIR/$NODE -- vtysh -E  "
-		ARGUMENT+="-c 'conf t'"
-		while read line 
-		do
-			ARGUMENT+=" -c '$line'"
-		done < "$file"
-		echo  $ARGUMENT >> start.sh
-	else
-		while read line
-		do
-			echo "/usr/sbin/vcmd -c $SOCKETS_DIR/$NODE -- bash -E -c '$line'" >> start.sh
-		done < "$file"
-	fi
+# Valido parámetros
+if [ $# -eq 0 ];
+then
+	echo "Debe especificar la ruta de destino." && exit 1
+fi
+
+# Obtengo instancia de CORE (por ahora sólo una)
+SOCKETS_DIR=$(ps aux | grep -oP "/tmp/pycore.[0-9]+" | head -n 1)
+
+# Ruta pasada como argumento
+CONFIGS_DIR=$1
+
+# Chequea si es el directio actual
+# o la ruta es relativa.
+# Sino deja la absoluta pasada por parámetro
+if [ "${#CONFIGS_DIR}" -eq "1" ] && [ "${CONFIGS_DIR:0:1}" = "." ];
+then
+	# es directorio actual
+	CONFIGS_DIR="$PWD/"
+elif [ "${CONFIGS_DIR:0:1}" != "/" ]; then
+	# es relativa
+	CONFIGS_DIR="${PWD}/$CONFIGS_DIR/"
+fi
+
+function ok() {
+	echo -e "$1: \e[32mOK\e[0m"
+}
+
+function fail() {
+	echo -e "$1: \e[31mFAIL\e[0m"
+}
+
+for file in $CONFIGS_DIR/*; do
+	filename=$(basename "$file")
+	node_type="${filename##*.}"
+	node=${filename%.*}
+
+	# Comando Base para comunicarme con los sockets
+	cmd="/usr/sbin/vcmd -c $SOCKETS_DIR/$node --"
+
+	case $node_type in
+		"router")
+			# Si es un router usaré vtysh
+			cmd="$cmd vtysh -E -c 'conf t' "
+			;;
+		"host")
+			# Si es un host bash
+			cmd="$cmd bash -E "
+			;;
+	esac
+
+	# Concateno los comandos a ejecutar
+	while read line; do
+		cmd+=" -c '$line'"
+	done < "$file"
+
+	# Ejecuto el comando de un tiro
+	eval $cmd &>/dev/null && ok $file || fail $file
 done
-bash start.sh
-rm start.sh
-
